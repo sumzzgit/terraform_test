539ae7e4912286819efa8784ec0614e995158ae4
sumzz <sumitkotabagi@gmial.com>
2024-02-04 02:08:44 +0530

    third commit

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,7 +5,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sumzzbucket1"
+    bucket = "sumzzbucket11"
     key = "terraform/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "terraform-statelocking"
